a82fad7c210e47429644445102a2ce6f63c0cc7b
Kiki7792 <921970613@qq.com>
2019-03-21 17:13:53 +0800

    不使用keepalive - extends

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <!-- exclude的作用是除了Detail.vue其他页面都会被缓存 -->
-    <keep-alive exclude="Detail">
+    <!-- exclude的作用是除了Detail.vue其他页面都会被缓存  exclude="Detail" 这样一做的话, Detail页面的header-fix就有问题了 -->
+    <keep-alive>
       <router-view></router-view>
     </keep-alive>
   </div>
--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <detail-banner
-     :sightName="sightName"
-     :bannerImg="bannerImg"
-     :bannerImgs="gallaryImgs"
-    >
-    </detail-banner>
+    <detail-banner :sightName="sightName" :bannerImg="bannerImg" :bannerImgs="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -17,7 +12,7 @@ import DetailBanner from "./components/Banner";
 import DetailHeader from "./components/Header";
 import DetailList from "./components/List";
 
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "Detail",
   components: {
@@ -27,37 +22,48 @@ export default {
   },
   data() {
     return {
-      sightName: '',
-      bannerImg: '',
+      sightName: "",
+      bannerImg: "",
       gallaryImgs: [],
-      list: []
+      list: [],
+      lastId: this.$route.params.id
     };
   },
   methods: {
     getDetailInfo() {
       // 因为id是动态添加进去的, 所以可以通过this.$route.params.id获取添加的id
       // axios.get("http://localhost:8080/mock/detail.json?id="+this.$route.params.id) //与下面同样有效
-      axios.get("http://localhost:8080/mock/detail.json", {
-        params: {
-          id: this.$route.params.id
-        }
-      }).then(this.handleGetDataSucc)
+      axios
+        .get("http://localhost:8080/mock/detail.json", {
+          params: {
+            id: this.$route.params.id
+          }
+        })
+        .then(this.handleGetDataSucc);
     },
     handleGetDataSucc(res) {
       // console.log(res);
-      res = res.data
-      if(res.ret && res.data) {
-        const data = res.data
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
         // console.log(data);
-        this.sightName = data.sightName
-        this.bannerImg = data.bannerImg
-        this.gallaryImgs = data.gallaryImgs
-        this.list = data.categoryList
+        this.sightName = data.sightName;
+        this.bannerImg = data.bannerImg;
+        this.gallaryImgs = data.gallaryImgs;
+        this.list = data.categoryList;
       }
     }
   },
   mounted() {
     this.getDetailInfo();
+  },
+  // APP.vue使用keepalive缓存请求数据, 但是同时多了一个生命周期函数 activated 改变keep-alive缓存中的数据
+  activated() {
+    //判断当前项目id, 与选择项目的id是否相同, 如果不相同就再次发送ajax请求
+    if (this.lastCity !== this.$route.params.id) {
+      this.lastCity = this.$route.params.id;
+      this.getDetailInfo();
+    }
   }
 };
 </script>
